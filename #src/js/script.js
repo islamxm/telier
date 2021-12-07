99f208f1f095b6e31dc7a7b4aa1a4fd4fa8ca740
Islam <isxmad@gmail.com>
2021-12-07 20:21:38 +0300

    asd

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const prevSwiper = new Swiper('.prev__in', {
-        autoplay: {
-            delay: 5000,
+        // autoplay: {
+        //     delay: 5000,
 
-        },
+        // },
         loop: true,
         speed: 1000,
         spaceBetween: 36,
@@ -34,9 +34,48 @@ document.addEventListener('DOMContentLoaded', () => {
             prevEl: '.product_nav_left',
         }
     });
+
+
+
+    const menuTrigger = document.querySelector('#menuTrigger');
+    const mainCatalog = document.querySelector('#mainMenu');
+
+    if(menuTrigger) {
+        menuTrigger.addEventListener('click', (e) => {
+            menuTrigger.classList.toggle('active');
+
+            mainCatalog.classList.toggle('active');
+        });
+    }
+
+
+    function dropDown(itemQuery, itemListQuery) {
+        let dropDownItem = document.querySelector(itemQuery);
+
+        if(dropDownItem) {
+            dropDownItem.addEventListener('click', (e) => {
+                let dropDownList = e.currentTarget.querySelector(itemListQuery);
+                let target = e.target;
+
+                if(target.hasAttribute('data-droplistitem')) {
+                    dropDownItem.classList.remove('active');
+                }
+                if(target && e.target.hasAttribute('data-drophead')) {
+                    dropDownItem.classList.toggle('active');
+
+                    if(dropDownItem.classList.contains('active')) {
+                        dropDownList.style.height = `${dropDownList.scrollHeight}px`;
+                    }
+                    if(!dropDownItem.classList.contains('active')) {
+                        dropDownList.style.height = `0px`;
+                    }
+                }
+
+            });
+        }
+    }
+
+    dropDown('.header__main_search_drop', '.header__main_search_drop_list');
 });
-
-
-
 
 
